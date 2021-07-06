4f0507804c43f670322bb79099b2fe39c113a0b7
Nassim <nassim.amokrane@hotmail.com>
2021-07-06 15:39:56 +0100

    ➕ add test utils

---
+++ src/utils/test-utlis.tsx
@@ -0,0 +1,31 @@
+// test-utils.js
+import React, { ComponentType, ReactChild, ReactChildren } from 'react';
+import { render } from '@testing-library/react';
+import { ToastProvider } from 'react-toast-notifications';
+import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+interface AuxProps {
+  children: ReactChild | ReactChildren;
+}
+
+const queryClient = new QueryClient();
+
+function renderWithTheme(ui: React.ReactElement) {
+  const Wrapper = ({ children }: AuxProps) => (
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider>
+        {children}
+      </ToastProvider>
+    </QueryClientProvider>
+  );
+  return {
+    ...render(ui, { wrapper: Wrapper as ComponentType }),
+  };
+}
+
+// override render method
+
+export * from '@testing-library/react';
+
+export { renderWithTheme };
